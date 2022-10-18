b92386d8e8485ac88f6084ec3487fc2ed43cd7d0
Sebastian Galvao <sebastian.galvao@genesis.global>
2022-10-18 19:20:53 -0300

    Upgrade do web ui 5.x

--- client/web/src/routes/home/home.template.ts
+++ client/web/src/routes/home/home.template.ts
@@ -1,8 +1,7 @@
 import {html, repeat, when, ref} from '@microsoft/fast-element';
 import type {Home} from './home';
-import {ColDef} from '@ag-grid-community/core';
 
-export const positionsColumnDefs: ColDef[] = [
+export const positionsColumnDefs: any[] = [
     {field: 'INSTRUMENT_ID', headerName: 'Instrument'},
     {field: 'QUANTITY', headerName: 'Quantity'},
     {field: 'NOTIONAL', headerName: 'Notional'},
@@ -27,14 +26,14 @@ export const HomeTemplate = html<Home>`
     <div class="top-layout">
         <zero-card class="positions-card">
             <span class="card-title">Positions</span>
-            <zero-ag-grid ${ref('positionsGrid')} rowHeight="45" only-template-col-defs>
+            <zero-grid-pro ${ref('positionsGrid')} rowHeight="45" only-template-col-defs>
                 ${when(x => x.connection.isConnected, html`
-                  <ag-genesis-datasource resourceName="ALL_POSITIONS"></ag-genesis-datasource>
+                  <grid-pro-genesis-datasource resourceName="ALL_POSITIONS"></grid-pro-genesis-datasource>
                   ${repeat(() => positionsColumnDefs, html`
-                    <ag-grid-column :definition="${x => x}" />
+                    <grid-pro-column :definition="${x => x}" />
                   `)}
                 `)}
-            </zero-ag-grid>
+            </zero-grid-pro>
         </zero-card>
     </div>
 </div>
