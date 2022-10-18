fe01cdbe217a5b476fbc3d3aba9e01607927d510
dbarros1979 <daniel.barros@genesis.global>
2022-10-18 19:24:57 +0000

    Updating to GSF 6.1.7

--- server/jvm/alpha-site-specific/src/main/resources/cfg/genesis-system-definition.kts
+++ server/jvm/alpha-site-specific/src/main/resources/cfg/genesis-system-definition.kts
@@ -25,7 +25,6 @@ systemDefinition {
 
         item(name = "ADMIN_PERMISSION_ENTITY_TABLE", value = "COUNTERPARTY")
         item(name = "ADMIN_PERMISSION_ENTITY_FIELD", value = "COUNTERPARTY_ID")
-
     }
 
     systems {
