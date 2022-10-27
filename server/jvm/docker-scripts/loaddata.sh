b089b12e3fc6d7bb69a521d8e413bdc0f9722709
sebastiangalvao <89458693+sebastiangalvao@users.noreply.github.com>
2022-10-27 10:48:35 -0300

    improve docker build

--- server/jvm/docker-scripts/loaddata.sh
+++ server/jvm/docker-scripts/loaddata.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /home/alpha/.bashrc
 systemctl start postgresql-14
-cd /home/alpha/run/site-specific/data
 su -c "source /home/alpha/.bashrc ; cd /home/alpha/run/site-specific/data ; yes | JvmRun global.genesis.environment.scripts.SendTable -a" - "alpha"
 echo "data loaded"
