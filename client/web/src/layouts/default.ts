d38050cff67dba285bc56d419ec18caebce711db
Sebastian Galvao <sebastian.galvao@genesis.global>
2022-11-24 14:14:06 -0300

    applying the code developed during the training, exercises etc

--- client/web/src/layouts/default.ts
+++ client/web/src/layouts/default.ts
@@ -47,58 +47,8 @@ export const defaultLayout = new FASTElementLayout(
             </zero-button>
           `
         )}
-        <span slot="group-title-1">GROUP SLOT</span>
-        <zero-tree-view slot="nav-items-1">
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-        </zero-tree-view>
-        <span slot="group-title-2">GROUP SLOT 2</span>
-        <zero-tree-view slot="nav-items-2">
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-        </zero-tree-view>
-        <span slot="group-title-3">GROUP SLOT 3</span>
-        <zero-tree-view slot="nav-items-3">
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            User Slot
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Reporting Slot
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Settings Slot
-          </zero-tree-item>
+        <!-- add items here -->
+        
         </zero-tree-view>
       </foundation-header>
       <div class="content">
