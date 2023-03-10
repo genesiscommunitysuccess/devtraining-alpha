b1bd68dacfc702b6665d44cbe8bc868e22cc5f3f
dbarros1979 <daniel.barros@genesis.global>
2023-03-10 20:53:33 +0000

    Formatting EntityManagement micro-frontend

--- client/web/src/routes/home/home.styles.ts
+++ client/web/src/routes/home/home.styles.ts
@@ -1,6 +1,40 @@
-import { css } from '@microsoft/fast-element';
+import {css} from '@microsoft/fast-element';
+import {mixinScreen} from '../../styles';
 
 export const HomeStyles = css`
   :host {
+    ${mixinScreen('flex')}
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    --neutral-stroke-divider-rest: var(--neutral-fill-stealth-rest);
+  }
+
+  .split-layout {
+      flex-direction: column;
+      flex: 1;
+      width: 100%;
+  }
+
+  .top-layout {
+  height: 90%;
+  flex-direction: row;
+  }
+
+  .positions-card {
+  flex: 1;
+  margin: calc(var(--design-unit) * 3px);
+  }
+
+  .card-title {
+  padding: calc(var(--design-unit) * 3px);
+  background-color: #22272a;
+  font-size: 13px;
+  font-weight: bold;
+  }
+
+  zero-ag-grid {
+  width: 100%;
+  height: 100%;
   }
 `;
